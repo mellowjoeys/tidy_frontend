c741fc526f210253a9647c796772f5d66af2c90c
mellowjoeys <mellowjoey@gmail.com>
2019-11-04 15:14:15 -0600

    adding 'suggest value' form feature, IN PROGRESS

--- src/views/Suggestions.vue
+++ src/views/Suggestions.vue
@@ -3,20 +3,33 @@
     <h1>Suggestions</h1>
     <div class="home">
       <div v-for="chore in house.chores">
-        <h2>{{ chore.name }}</h2>
-          
-        <div v-for="suggestion in chore.suggestions">
-          <h4>{{ suggestion }} </h4>
+        <h2 v-on:click="showChore(chore)">{{ chore.name }}</h2>
+          <p>Value: {{ chore.value }}</p>
+          </p>
 
+
+        <div v-if="chore === currentChore">
+          <p>My Suggested Value: 
+          <input type="text" v-model="mySuggestedValue">
+          <div v-for="suggestion in chore.suggestions">
+            <h4>{{ suggestion }} </h4>
+          </div>   
+          <div class="edit-form-section">
+            <h4>Suggest a value</h4>
+
+            <div>
+              Value: <input type="number" v-model="chore.value">
+            </div>
+              
+            <button v-on:click="suggestValue(chore)">Suggest</button>
+          </div><!-- end of .edit-form-section -->
         </div>
+
+
       </div>
       <router-link v-bind:to="'/todo'">
         <h2>To Do</h2>
-
       </router-link>
-    </div>
-    <div>
-      {{ currentUser.house_id }}
     </div>
   </div>
 </template>
@@ -31,8 +44,10 @@ export default {
   data: function() {
     return {
       currentUser: {},
-      house: [],
-      suggestions: []
+      house: {},
+      currentChore: {},
+      mySuggestedValue: 0,
+      newValue: 0
     };
   },
   created: function() {
@@ -47,6 +62,25 @@ export default {
       })
   },
   methods: {
+    showChore: function(choreObject) {
+      if (this.currentChore != choreObject) {
+        this.currentChore = choreObject;
+        this.currentChore.suggestions.forEach(function(suggestion) {
+          if (suggestion.user_id === this.currentUser.id) {
+            this.mySuggestedValue = suggestion.value
+          }
+        });
+      } else {
+        this.currentChore = {};
+        this.mySuggestedValue = 0;
+      }
+    }
+
+    // suggestValue: function(choreObject) {
+    //   //I want to update a value if mySuggestedValue is not 0. If it is, create a suggestion. 
+    //   axios
+    //     .post("")
+    // }
   }
 };
 </script>
