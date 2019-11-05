445547d77fa770f84b85a1d8a851eb33a8d32146
mellowjoeys <mellowjoey@gmail.com>
2019-11-05 16:56:31 -0600

    Adding created() and other methods to UserChoresCreate, WIP

---
+++ src/views/UserChoresCreate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="user-chores-new">
+    <h1>Tidy Time</h1>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios"
+
+export default {
+  data: function() {
+    return {
+      currentUser: {},
+      availableChores: [], // user can refresh and see updated list, then use action cables to auto refresh. 
+      chosenChores: [],
+      housemates: [],
+      housematesValues: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/users/current")
+      .then(response => {
+        this.currentUser = response.data;
+      });
+  },
+  methods: {
+  }
+};
+</script>
